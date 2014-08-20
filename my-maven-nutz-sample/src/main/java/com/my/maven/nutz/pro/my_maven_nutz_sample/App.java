4af2d5bfaa89b47a7047bbeabed1fb967f08e929
WaterHsu <WaterHsu@WaterHsu-PC>
2014-08-20 19:03:39 +0800

    first commit

---
+++ my-maven-nutz-sample/src/main/java/com/my/maven/nutz/pro/my_maven_nutz_sample/App.java
@@ -0,0 +1,13 @@
+package com.my.maven.nutz.pro.my_maven_nutz_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
