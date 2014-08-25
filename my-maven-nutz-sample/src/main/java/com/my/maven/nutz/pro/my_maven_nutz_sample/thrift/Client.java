488255a020ff6c4a408a5f96800310fa42cbaf6a
WaterHsu <WaterHsu@WaterHsu-PC>
2014-08-25 14:22:25 +0800

    Thrift的Client和Server端实现

--- my-maven-nutz-sample/src/main/java/com/my/maven/nutz/pro/my_maven_nutz_sample/thrift/Client.java
+++ my-maven-nutz-sample/src/main/java/com/my/maven/nutz/pro/my_maven_nutz_sample/thrift/Client.java
@@ -4,10 +4,40 @@
  */
 package com.my.maven.nutz.pro.my_maven_nutz_sample.thrift;
 
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
 /**
  * @author WaterHsu@xiu8.com
  * @version 2014年8月22日
  */
 public class Client {
-
+	public void startClient(){
+		TTransport transport;
+		try{
+			System.out.println("thrift client connect server at 1234 port");
+			transport = new TSocket("localhost", 1234);
+			TProtocol protocol = new TBinaryProtocol(transport);
+			Hello.Client client = new Hello.Client(protocol);
+			transport.open();
+			System.out.println(client.helloString("Hello World in Thrift!"));
+			transport.close();
+			System.out.println("thrift client close connection!");
+		}catch(TTransportException e){
+			e.printStackTrace();
+		}catch(TException e){
+			e.printStackTrace();
+		}
+	}
+	public static void main(String args[]){
+		System.out.println("thrift client init");
+		Client client = new Client();
+		System.out.println("thrift client start");
+		client.startClient();
+		System.out.println("thrift client end");
+	}
 }
--- my-maven-nutz-sample/src/main/java/com/my/maven/nutz/pro/my_maven_nutz_sample/thrift/Server.java
+++ my-maven-nutz-sample/src/main/java/com/my/maven/nutz/pro/my_maven_nutz_sample/thrift/Server.java
@@ -6,6 +6,9 @@ package com.my.maven.nutz.pro.my_maven_nutz_sample.thrift;
 
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TBinaryProtocol.Factory;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.server.TThreadPoolServer.Args;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
 
@@ -22,9 +25,22 @@ public class Server {
 			System.out.println("thrift server open port 1234");
 			TServerSocket serverTransport = new TServerSocket(1234);
 			Hello.Processor processor = new Processor(new HelloImpl());
-//			Factory portFactory = new TBinaryProtocol().Factory(true, true);
+			Factory portFactory = new TBinaryProtocol.Factory(true, true);
+			Args args = new Args(serverTransport);
+			args.processor(processor);
+			args.protocolFactory(portFactory);
+			TServer server = new TThreadPoolServer(args);
+			server.serve();
 		}catch(TTransportException e){
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String args[]){
+		System.out.println("thrift server init");
+		Server server =  new Server();
+		System.out.println("thrift server start");
+		server.startServer();
+		System.out.println("thrift server end");
 	}
 }
