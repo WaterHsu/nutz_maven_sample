01fa9553b2db455dca9a5e34cbc14530fc8c8f1b
大雄 <hsupangfei1989@gmail.com>
2014-09-01 14:25:09 +0800

    公式比赛  大文件处理

---
+++ deal/src/main/java/competition_bigfile/deal/App.java
@@ -0,0 +1,13 @@
+package competition_bigfile.deal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
